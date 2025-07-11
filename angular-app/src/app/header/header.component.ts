7575df7f73063421ca9cf93195169525cbcb967a
thomas.bee <thomas.bee@twikey.com>
2025-07-11 19:09:25 +0200

    cool updates

--- angular-app/src/app/header/header.component.ts
+++ angular-app/src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LogoComponent } from '../logo/logo.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, LogoComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
